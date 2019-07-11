2c4fa91ba2aa3d0ebc6d9a2ea434ecbafbf6c4d5
Néfix Estrada <nefixestrada@gmail.com>
2019-07-11 08:01:48 +0000

    Add certificate generation test util
    Now there's a new test util that generates a certificate and stores it
    in a FS. This is really useful for testing functions that require TLS
    stuff.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/golang/protobuf v1.3.1
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/afero v1.1.2
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.2.2
 	google.golang.org/grpc v1.22.0
---
+++ utils/tests/cert_test.go
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 DRLM Project
+ * Authors: DRLM Common authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package tests_test
+
+import (
+	"testing"
+
+	"github.com/brainupdaters/drlm-common/utils/tests"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateCert(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("should work as expected", func(t *testing.T) {
+		fs := afero.NewMemMapFs()
+
+		err := tests.GenerateCert("testcert", fs, "/etc/certs")
+		assert.Nil(err)
+
+		const keyFile string = "/etc/certs/testcert.key"
+		const crtFile string = "/etc/certs/testcert.crt"
+
+		assert.True(afero.Exists(fs, keyFile))
+		assert.True(afero.Exists(fs, crtFile))
+
+		key, err := afero.ReadFile(fs, keyFile)
+		assert.Nil(err)
+		assert.NotEmpty(key)
+
+		crt, err := afero.ReadFile(fs, crtFile)
+		assert.Nil(err)
+		assert.NotEmpty(crt)
+	})
+}
