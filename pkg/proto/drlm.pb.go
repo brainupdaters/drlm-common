b3f1ca49475a0e9733ea7d21ed4369f7b4e0e32b
Néfix Estrada <nefixestrada@gmail.com>
2019-07-22 06:04:00 +0000

    Added token expiration times. Removed api versions
    - Now, when a new token is issued, the expiration time is also attached,
      making the client able to choose whether to renew the token or wait a
      bit more since it still can be used.
    - Also, removed the API versioning from the requests. The API version
      should be sent as metadata, not as an actual request parameter.

--- pkg/proto/drlm.pb.go
+++ pkg/proto/drlm.pb.go
@@ -7,6 +7,7 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	timestamp "github.com/golang/protobuf/ptypes/timestamp"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -25,7 +26,6 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type UserLoginRequest struct {
-	Api                  string   `protobuf:"bytes,1,opt,name=api,proto3" json:"api,omitempty"`
 	Usr                  string   `protobuf:"bytes,2,opt,name=usr,proto3" json:"usr,omitempty"`
 	Pwd                  string   `protobuf:"bytes,3,opt,name=pwd,proto3" json:"pwd,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -58,13 +58,6 @@ func (m *UserLoginRequest) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_UserLoginRequest proto.InternalMessageInfo
 
-func (m *UserLoginRequest) GetApi() string {
-	if m != nil {
-		return m.Api
-	}
-	return ""
-}
-
 func (m *UserLoginRequest) GetUsr() string {
 	if m != nil {
 		return m.Usr
@@ -80,10 +73,11 @@ func (m *UserLoginRequest) GetPwd() string {
 }
 
 type UserLoginResponse struct {
-	Tkn                  string   `protobuf:"bytes,1,opt,name=tkn,proto3" json:"tkn,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Tkn                  string               `protobuf:"bytes,1,opt,name=tkn,proto3" json:"tkn,omitempty"`
+	TknExpiration        *timestamp.Timestamp `protobuf:"bytes,2,opt,name=tkn_expiration,json=tknExpiration,proto3" json:"tkn_expiration,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
+	XXX_unrecognized     []byte               `json:"-"`
+	XXX_sizecache        int32                `json:"-"`
 }
 
 func (m *UserLoginResponse) Reset()         { *m = UserLoginResponse{} }
@@ -118,8 +112,92 @@ func (m *UserLoginResponse) GetTkn() string {
 	return ""
 }
 
+func (m *UserLoginResponse) GetTknExpiration() *timestamp.Timestamp {
+	if m != nil {
+		return m.TknExpiration
+	}
+	return nil
+}
+
+type UserTokenRenewRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *UserTokenRenewRequest) Reset()         { *m = UserTokenRenewRequest{} }
+func (m *UserTokenRenewRequest) String() string { return proto.CompactTextString(m) }
+func (*UserTokenRenewRequest) ProtoMessage()    {}
+func (*UserTokenRenewRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a4bd9cd91f607bb1, []int{2}
+}
+
+func (m *UserTokenRenewRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_UserTokenRenewRequest.Unmarshal(m, b)
+}
+func (m *UserTokenRenewRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_UserTokenRenewRequest.Marshal(b, m, deterministic)
+}
+func (m *UserTokenRenewRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UserTokenRenewRequest.Merge(m, src)
+}
+func (m *UserTokenRenewRequest) XXX_Size() int {
+	return xxx_messageInfo_UserTokenRenewRequest.Size(m)
+}
+func (m *UserTokenRenewRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_UserTokenRenewRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UserTokenRenewRequest proto.InternalMessageInfo
+
+type UserTokenRenewResponse struct {
+	Tkn                  string               `protobuf:"bytes,1,opt,name=tkn,proto3" json:"tkn,omitempty"`
+	TknExpiration        *timestamp.Timestamp `protobuf:"bytes,2,opt,name=tkn_expiration,json=tknExpiration,proto3" json:"tkn_expiration,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
+	XXX_unrecognized     []byte               `json:"-"`
+	XXX_sizecache        int32                `json:"-"`
+}
+
+func (m *UserTokenRenewResponse) Reset()         { *m = UserTokenRenewResponse{} }
+func (m *UserTokenRenewResponse) String() string { return proto.CompactTextString(m) }
+func (*UserTokenRenewResponse) ProtoMessage()    {}
+func (*UserTokenRenewResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a4bd9cd91f607bb1, []int{3}
+}
+
+func (m *UserTokenRenewResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_UserTokenRenewResponse.Unmarshal(m, b)
+}
+func (m *UserTokenRenewResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_UserTokenRenewResponse.Marshal(b, m, deterministic)
+}
+func (m *UserTokenRenewResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UserTokenRenewResponse.Merge(m, src)
+}
+func (m *UserTokenRenewResponse) XXX_Size() int {
+	return xxx_messageInfo_UserTokenRenewResponse.Size(m)
+}
+func (m *UserTokenRenewResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_UserTokenRenewResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UserTokenRenewResponse proto.InternalMessageInfo
+
+func (m *UserTokenRenewResponse) GetTkn() string {
+	if m != nil {
+		return m.Tkn
+	}
+	return ""
+}
+
+func (m *UserTokenRenewResponse) GetTknExpiration() *timestamp.Timestamp {
+	if m != nil {
+		return m.TknExpiration
+	}
+	return nil
+}
+
 type UserAddRequest struct {
-	Api                  string   `protobuf:"bytes,1,opt,name=api,proto3" json:"api,omitempty"`
 	Usr                  string   `protobuf:"bytes,2,opt,name=usr,proto3" json:"usr,omitempty"`
 	Pwd                  string   `protobuf:"bytes,3,opt,name=pwd,proto3" json:"pwd,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -131,7 +209,7 @@ func (m *UserAddRequest) Reset()         { *m = UserAddRequest{} }
 func (m *UserAddRequest) String() string { return proto.CompactTextString(m) }
 func (*UserAddRequest) ProtoMessage()    {}
 func (*UserAddRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{2}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{4}
 }
 
 func (m *UserAddRequest) XXX_Unmarshal(b []byte) error {
@@ -151,13 +229,6 @@ func (m *UserAddRequest) XXX_DiscardUnknown() {
 }
 
 var xxx_messageInfo_UserAddRequest proto.InternalMessageInfo
-
-func (m *UserAddRequest) GetApi() string {
-	if m != nil {
-		return m.Api
-	}
-	return ""
-}
 
 func (m *UserAddRequest) GetUsr() string {
 	if m != nil {
@@ -183,7 +254,7 @@ func (m *UserAddResponse) Reset()         { *m = UserAddResponse{} }
 func (m *UserAddResponse) String() string { return proto.CompactTextString(m) }
 func (*UserAddResponse) ProtoMessage()    {}
 func (*UserAddResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{3}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{5}
 }
 
 func (m *UserAddResponse) XXX_Unmarshal(b []byte) error {
@@ -205,7 +276,6 @@ func (m *UserAddResponse) XXX_DiscardUnknown() {
 var xxx_messageInfo_UserAddResponse proto.InternalMessageInfo
 
 type UserDeleteRequest struct {
-	Api                  string   `protobuf:"bytes,1,opt,name=api,proto3" json:"api,omitempty"`
 	Usr                  string   `protobuf:"bytes,2,opt,name=usr,proto3" json:"usr,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -216,7 +286,7 @@ func (m *UserDeleteRequest) Reset()         { *m = UserDeleteRequest{} }
 func (m *UserDeleteRequest) String() string { return proto.CompactTextString(m) }
 func (*UserDeleteRequest) ProtoMessage()    {}
 func (*UserDeleteRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{4}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{6}
 }
 
 func (m *UserDeleteRequest) XXX_Unmarshal(b []byte) error {
@@ -237,13 +307,6 @@ func (m *UserDeleteRequest) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_UserDeleteRequest proto.InternalMessageInfo
 
-func (m *UserDeleteRequest) GetApi() string {
-	if m != nil {
-		return m.Api
-	}
-	return ""
-}
-
 func (m *UserDeleteRequest) GetUsr() string {
 	if m != nil {
 		return m.Usr
@@ -261,7 +324,7 @@ func (m *UserDeleteResponse) Reset()         { *m = UserDeleteResponse{} }
 func (m *UserDeleteResponse) String() string { return proto.CompactTextString(m) }
 func (*UserDeleteResponse) ProtoMessage()    {}
 func (*UserDeleteResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{5}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{7}
 }
 
 func (m *UserDeleteResponse) XXX_Unmarshal(b []byte) error {
@@ -283,7 +346,6 @@ func (m *UserDeleteResponse) XXX_DiscardUnknown() {
 var xxx_messageInfo_UserDeleteResponse proto.InternalMessageInfo
 
 type UserListRequest struct {
-	Api                  string   `protobuf:"bytes,1,opt,name=api,proto3" json:"api,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -293,7 +355,7 @@ func (m *UserListRequest) Reset()         { *m = UserListRequest{} }
 func (m *UserListRequest) String() string { return proto.CompactTextString(m) }
 func (*UserListRequest) ProtoMessage()    {}
 func (*UserListRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{6}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{8}
 }
 
 func (m *UserListRequest) XXX_Unmarshal(b []byte) error {
@@ -314,13 +376,6 @@ func (m *UserListRequest) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_UserListRequest proto.InternalMessageInfo
 
-func (m *UserListRequest) GetApi() string {
-	if m != nil {
-		return m.Api
-	}
-	return ""
-}
-
 type UserListResponse struct {
 	Users                []*UserListResponse_User `protobuf:"bytes,1,rep,name=users,proto3" json:"users,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}                 `json:"-"`
@@ -332,7 +387,7 @@ func (m *UserListResponse) Reset()         { *m = UserListResponse{} }
 func (m *UserListResponse) String() string { return proto.CompactTextString(m) }
 func (*UserListResponse) ProtoMessage()    {}
 func (*UserListResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{7}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{9}
 }
 
 func (m *UserListResponse) XXX_Unmarshal(b []byte) error {
@@ -371,7 +426,7 @@ func (m *UserListResponse_User) Reset()         { *m = UserListResponse_User{} }
 func (m *UserListResponse_User) String() string { return proto.CompactTextString(m) }
 func (*UserListResponse_User) ProtoMessage()    {}
 func (*UserListResponse_User) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a4bd9cd91f607bb1, []int{7, 0}
+	return fileDescriptor_a4bd9cd91f607bb1, []int{9, 0}
 }
 
 func (m *UserListResponse_User) XXX_Unmarshal(b []byte) error {
@@ -402,6 +457,8 @@ func (m *UserListResponse_User) GetUsr() string {
 func init() {
 	proto.RegisterType((*UserLoginRequest)(nil), "drlm.UserLoginRequest")
 	proto.RegisterType((*UserLoginResponse)(nil), "drlm.UserLoginResponse")
+	proto.RegisterType((*UserTokenRenewRequest)(nil), "drlm.UserTokenRenewRequest")
+	proto.RegisterType((*UserTokenRenewResponse)(nil), "drlm.UserTokenRenewResponse")
 	proto.RegisterType((*UserAddRequest)(nil), "drlm.UserAddRequest")
 	proto.RegisterType((*UserAddResponse)(nil), "drlm.UserAddResponse")
 	proto.RegisterType((*UserDeleteRequest)(nil), "drlm.UserDeleteRequest")
@@ -414,26 +471,32 @@ func init() {
 func init() { proto.RegisterFile("drlm.proto", fileDescriptor_a4bd9cd91f607bb1) }
 
 var fileDescriptor_a4bd9cd91f607bb1 = []byte{
-	// 303 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xa4, 0x92, 0xcf, 0x4b, 0xc3, 0x30,
-	0x14, 0xc7, 0xd7, 0xb5, 0xfe, 0xd8, 0x13, 0x74, 0x0b, 0xdb, 0x0c, 0xf5, 0x32, 0x22, 0xc2, 0x4e,
-	0x03, 0xe7, 0x41, 0x41, 0x10, 0x06, 0x43, 0x3c, 0xd4, 0x4b, 0xc1, 0xb3, 0x28, 0x09, 0x52, 0x36,
-	0xdb, 0x98, 0xa4, 0xec, 0xea, 0x9f, 0x2e, 0x69, 0xd2, 0x34, 0xb6, 0x7a, 0x10, 0x6f, 0xe9, 0xb7,
-	0xdf, 0xf7, 0x7d, 0xef, 0x7d, 0x12, 0x00, 0x2a, 0xb6, 0xef, 0x0b, 0x2e, 0x0a, 0x55, 0xa0, 0x48,
-	0x9f, 0xc9, 0x03, 0x0c, 0x9f, 0x24, 0x13, 0x49, 0xf1, 0x96, 0xe5, 0x29, 0xfb, 0x28, 0x99, 0x54,
-	0x68, 0x08, 0xe1, 0x0b, 0xcf, 0x70, 0x30, 0x0b, 0xe6, 0x83, 0x54, 0x1f, 0xb5, 0x52, 0x4a, 0x81,
-	0xfb, 0x46, 0x29, 0xa5, 0xd0, 0x0a, 0xdf, 0x51, 0x1c, 0x1a, 0x85, 0xef, 0x28, 0xb9, 0x80, 0x91,
-	0x97, 0x24, 0x79, 0x91, 0x4b, 0xa6, 0x6d, 0x6a, 0x93, 0xd7, 0x51, 0x6a, 0x93, 0x93, 0x7b, 0x38,
-	0xd6, 0xb6, 0x15, 0xa5, 0xff, 0x6b, 0x37, 0x82, 0x13, 0x97, 0x63, 0x9a, 0x91, 0x6b, 0x33, 0xc1,
-	0x9a, 0x6d, 0x99, 0x62, 0x7f, 0x48, 0x27, 0x63, 0x40, 0x7e, 0xa1, 0x8d, 0x3b, 0x37, 0x1d, 0x92,
-	0x4c, 0xaa, 0x5f, 0xc3, 0xc8, 0xb3, 0xe5, 0x57, 0x99, 0xec, 0xd2, 0x97, 0xb0, 0x57, 0x4a, 0x26,
-	0x24, 0x0e, 0x66, 0xe1, 0xfc, 0x68, 0x79, 0xb6, 0xa8, 0xa8, 0xb7, 0x6d, 0x95, 0x90, 0x1a, 0x67,
-	0x8c, 0x21, 0xd2, 0x9f, 0xf5, 0x6c, 0x81, 0x9b, 0x6d, 0xf9, 0xd9, 0x87, 0x68, 0x9d, 0x26, 0x8f,
-	0xe8, 0x0e, 0x06, 0x8e, 0x2f, 0x9a, 0x7a, 0x99, 0xde, 0xd5, 0xc5, 0xa7, 0x1d, 0xdd, 0x2e, 0xd3,
-	0x43, 0x37, 0x70, 0x60, 0x81, 0xa1, 0x71, 0xe3, 0x6a, 0xee, 0x21, 0x9e, 0xb4, 0x54, 0x57, 0xb9,
-	0x02, 0x68, 0xf0, 0x20, 0xaf, 0xc5, 0x37, 0xd2, 0x31, 0xee, 0xfe, 0x70, 0x11, 0xb7, 0x70, 0x58,
-	0xef, 0x8f, 0x26, 0x6d, 0x1e, 0xa6, 0x7c, 0xfa, 0x33, 0x26, 0xd2, 0x7b, 0xdd, 0xaf, 0x1e, 0xec,
-	0xd5, 0x57, 0x00, 0x00, 0x00, 0xff, 0xff, 0xb4, 0xa0, 0x76, 0x18, 0xbe, 0x02, 0x00, 0x00,
+	// 386 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x52, 0x4d, 0x4b, 0xf3, 0x40,
+	0x18, 0x6c, 0xda, 0xbe, 0xaf, 0xf6, 0x29, 0xd6, 0x76, 0xe9, 0x47, 0x88, 0x82, 0x25, 0x20, 0xf4,
+	0x94, 0x62, 0x15, 0x11, 0x04, 0xa1, 0x50, 0x6f, 0xed, 0x25, 0xd4, 0x73, 0x69, 0xc9, 0x63, 0x0d,
+	0x69, 0x76, 0x63, 0x76, 0x43, 0xfd, 0x97, 0xfe, 0x25, 0x49, 0x36, 0xd9, 0xc4, 0xb4, 0x1e, 0x3c,
+	0x78, 0x4b, 0x66, 0x67, 0x66, 0x87, 0x99, 0x05, 0x70, 0xc2, 0x9d, 0x6f, 0x05, 0x21, 0x13, 0x8c,
+	0xd4, 0xe3, 0x6f, 0xe3, 0x6a, 0xcb, 0xd8, 0x76, 0x87, 0xe3, 0x04, 0xdb, 0x44, 0xaf, 0x63, 0xe1,
+	0xfa, 0xc8, 0xc5, 0xda, 0x0f, 0x24, 0xcd, 0xbc, 0x87, 0xf6, 0x0b, 0xc7, 0x70, 0xce, 0xb6, 0x2e,
+	0xb5, 0xf1, 0x3d, 0x42, 0x2e, 0x48, 0x1b, 0x6a, 0x11, 0x0f, 0xf5, 0xea, 0x50, 0x1b, 0x35, 0xec,
+	0xf8, 0x33, 0x46, 0x82, 0xbd, 0xa3, 0xd7, 0x24, 0x12, 0xec, 0x1d, 0xf3, 0x0d, 0x3a, 0x05, 0x1d,
+	0x0f, 0x18, 0xe5, 0x18, 0xd3, 0x84, 0x47, 0x75, 0x4d, 0xd2, 0x84, 0x47, 0xc9, 0x14, 0x5a, 0xc2,
+	0xa3, 0x2b, 0xfc, 0x08, 0xdc, 0x70, 0x2d, 0x5c, 0x46, 0x13, 0xd7, 0xe6, 0xc4, 0xb0, 0x64, 0x30,
+	0x2b, 0x0b, 0x66, 0x2d, 0xb3, 0x60, 0xf6, 0x99, 0xf0, 0xe8, 0xb3, 0x12, 0x98, 0x03, 0xe8, 0xc5,
+	0x37, 0x2d, 0x99, 0x87, 0xd4, 0x46, 0x8a, 0xfb, 0x34, 0xa6, 0xe9, 0x43, 0xbf, 0x7c, 0xf0, 0x97,
+	0x39, 0xee, 0xa0, 0x15, 0x5f, 0x37, 0x75, 0x9c, 0xdf, 0xf4, 0xd4, 0x81, 0x73, 0xa5, 0x92, 0xe9,
+	0xcc, 0x6b, 0x59, 0xdd, 0x0c, 0x77, 0x28, 0xf0, 0x47, 0x2f, 0xb3, 0x0b, 0xa4, 0x48, 0x4b, 0xc5,
+	0xa9, 0xdf, 0xdc, 0xe5, 0x22, 0xeb, 0x61, 0x95, 0x4e, 0x98, 0x40, 0x69, 0x03, 0x37, 0xf0, 0x2f,
+	0xe2, 0x18, 0x72, 0x5d, 0x1b, 0xd6, 0x46, 0xcd, 0xc9, 0x85, 0x95, 0xbc, 0x8c, 0x32, 0x2d, 0x01,
+	0x6c, 0xc9, 0x34, 0x74, 0xa8, 0xc7, 0xbf, 0x59, 0x12, 0x4d, 0x25, 0x99, 0x7c, 0x56, 0xa1, 0x3e,
+	0xb3, 0xe7, 0x0b, 0xf2, 0x04, 0x0d, 0x35, 0x3a, 0xe9, 0x17, 0x3c, 0x0b, 0xaf, 0xc7, 0x18, 0x1c,
+	0xe0, 0x69, 0xf4, 0x0a, 0x59, 0xc8, 0x0a, 0xf3, 0xc5, 0x48, 0x21, 0xd8, 0xc1, 0xc0, 0xc6, 0xe5,
+	0xf1, 0x43, 0x65, 0xf7, 0x00, 0x27, 0x69, 0xb7, 0xa4, 0x9b, 0x53, 0xf3, 0x81, 0x8c, 0x5e, 0x09,
+	0x55, 0xca, 0x29, 0x40, 0xde, 0x2d, 0x29, 0x24, 0xfe, 0x36, 0x8a, 0xa1, 0x1f, 0x1e, 0x28, 0x8b,
+	0x47, 0x38, 0xcd, 0xea, 0x24, 0xbd, 0x72, 0xbd, 0x52, 0xde, 0x3f, 0xde, 0xba, 0x59, 0xd9, 0xfc,
+	0x4f, 0x9e, 0xdb, 0xed, 0x57, 0x00, 0x00, 0x00, 0xff, 0xff, 0xb0, 0x19, 0x48, 0xcb, 0xb1, 0x03,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -449,6 +512,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type DRLMClient interface {
 	UserLogin(ctx context.Context, in *UserLoginRequest, opts ...grpc.CallOption) (*UserLoginResponse, error)
+	UserTokenRenew(ctx context.Context, in *UserTokenRenewRequest, opts ...grpc.CallOption) (*UserTokenRenewResponse, error)
 	UserAdd(ctx context.Context, in *UserAddRequest, opts ...grpc.CallOption) (*UserAddResponse, error)
 	UserDelete(ctx context.Context, in *UserDeleteRequest, opts ...grpc.CallOption) (*UserDeleteResponse, error)
 	UserList(ctx context.Context, in *UserListRequest, opts ...grpc.CallOption) (*UserListResponse, error)
@@ -465,6 +529,15 @@ func NewDRLMClient(cc *grpc.ClientConn) DRLMClient {
 func (c *dRLMClient) UserLogin(ctx context.Context, in *UserLoginRequest, opts ...grpc.CallOption) (*UserLoginResponse, error) {
 	out := new(UserLoginResponse)
 	err := c.cc.Invoke(ctx, "/drlm.DRLM/UserLogin", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *dRLMClient) UserTokenRenew(ctx context.Context, in *UserTokenRenewRequest, opts ...grpc.CallOption) (*UserTokenRenewResponse, error) {
+	out := new(UserTokenRenewResponse)
+	err := c.cc.Invoke(ctx, "/drlm.DRLM/UserTokenRenew", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -501,6 +574,7 @@ func (c *dRLMClient) UserList(ctx context.Context, in *UserListRequest, opts ...
 // DRLMServer is the server API for DRLM service.
 type DRLMServer interface {
 	UserLogin(context.Context, *UserLoginRequest) (*UserLoginResponse, error)
+	UserTokenRenew(context.Context, *UserTokenRenewRequest) (*UserTokenRenewResponse, error)
 	UserAdd(context.Context, *UserAddRequest) (*UserAddResponse, error)
 	UserDelete(context.Context, *UserDeleteRequest) (*UserDeleteResponse, error)
 	UserList(context.Context, *UserListRequest) (*UserListResponse, error)
@@ -512,6 +586,9 @@ type UnimplementedDRLMServer struct {
 
 func (*UnimplementedDRLMServer) UserLogin(ctx context.Context, req *UserLoginRequest) (*UserLoginResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UserLogin not implemented")
+}
+func (*UnimplementedDRLMServer) UserTokenRenew(ctx context.Context, req *UserTokenRenewRequest) (*UserTokenRenewResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserTokenRenew not implemented")
 }
 func (*UnimplementedDRLMServer) UserAdd(ctx context.Context, req *UserAddRequest) (*UserAddResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UserAdd not implemented")
@@ -541,6 +618,24 @@ func _DRLM_UserLogin_Handler(srv interface{}, ctx context.Context, dec func(inte
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(DRLMServer).UserLogin(ctx, req.(*UserLoginRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DRLM_UserTokenRenew_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UserTokenRenewRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DRLMServer).UserTokenRenew(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/drlm.DRLM/UserTokenRenew",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DRLMServer).UserTokenRenew(ctx, req.(*UserTokenRenewRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -606,6 +701,10 @@ var _DRLM_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UserLogin",
 			Handler:    _DRLM_UserLogin_Handler,
+		},
+		{
+			MethodName: "UserTokenRenew",
+			Handler:    _DRLM_UserTokenRenew_Handler,
 		},
 		{
 			MethodName: "UserAdd",
