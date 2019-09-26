4554a09364e4da99abdd2a891e4ff3134b237e80
Néfix Estrada <nefixestrada@gmail.com>
2019-09-26 08:35:38 +0000

    Added the InstallBinary method

---
+++ pkg/os/cmdpkg.go
@@ -0,0 +1,18 @@
+package os
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+// CmdPkgInstallBinary installs a binary in a system
+func (os OS) CmdPkgInstallBinary(c Client, name string, b []byte) error {
+	switch os {
+	case Linux:
+		if err := os.CmdFSAppendToFileAdmin(c, filepath.Join("/usr/sbin", name), b); err != nil {
+			return fmt.Errorf("error installing the binary: %v", err)
+		}
+	default:
+		return ErrUnsupportedOS
+	}
+}
