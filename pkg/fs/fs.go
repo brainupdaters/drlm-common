a4f42e0dcd956d18b6f1aaae3d9368f1c5891d9b
Néfix Estrada <nefixestrada@gmail.com>
2019-11-20 10:22:58 +0000

    Added the SPDX license to all the code

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
 /*
  * Copyright (C) 2019 DRLM Project
  * Authors: DRLM Common authors
--- pkg/test/freeport_test.go
+++ pkg/test/freeport_test.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
 /*
  * Copyright (C) 2019 IsardVDI
  * Authors: IsardVDI Authors
