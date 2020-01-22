401ed993032b964b1b4f7459d93f7d9caec0f71b
Néfix Estrada <nefixestrada@gmail.com>
2020-01-22 16:57:22 +0000

    Added the CmdPkgWriteConfig function

--- pkg/os/cmdpkg.go
+++ pkg/os/cmdpkg.go
@@ -52,3 +52,35 @@ export PATH="$PATH:$HOME/.bin"
 		return ErrUnsupportedOS
 	}
 }
+
+// CmdPkgWriteConfig writes the configuration of a DRLM program
+func (os OS) CmdPkgWriteConfig(c client.Client, usr, name string, b []byte) error {
+	switch os {
+	case Linux:
+		home, err := os.CmdFSHome(c, usr)
+		if err != nil {
+			return fmt.Errorf("error installing the binary: %v", err)
+		}
+
+		cfgDir := filepath.Join(home, ".config", "drlm")
+		exists, err := c.Exists(cfgDir)
+		if err != nil {
+			return fmt.Errorf("error writting the configuration: %v", err)
+		}
+		if !exists {
+			if err = c.Mkdir(cfgDir, 0755); err != nil {
+				return fmt.Errorf("error creating the configuration directory: %v", err)
+			}
+		}
+
+		cfgPath := filepath.Join(cfgDir, name)
+		if err := c.Write(cfgPath, b); err != nil {
+			return fmt.Errorf("error writting the configuration file: %v", err)
+		}
+
+		return nil
+
+	default:
+		return ErrUnsupportedOS
+	}
+}
